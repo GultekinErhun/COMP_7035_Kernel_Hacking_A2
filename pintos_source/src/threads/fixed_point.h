05d79d17a3eb2f6c3c7de09f38176722f155160b
Erhun Gültekin <160606644+GultekinErhun@users.noreply.github.com>
2024-04-03 18:10:52 +0300

    Update fixed_point.h

--- pintos_source/src/threads/fixed_point.h
+++ pintos_source/src/threads/fixed_point.h
@@ -1,9 +1,8 @@
 #ifndef __THREAD_FIXED_POINT_H
 #define __THREAD_FIXED_POINT_H
 
-typedef int fixed_point;
-//yorum satiri
-#define FRACTION 15
+//typedef int fixed_point;
+//#define FRACTION 15
 // Convert x to integer (rounding toward zero):	x / f
 // convert x to integer rounding to nearest!
 // (x + f / 2) / f if x >= 0,
@@ -18,16 +17,35 @@ typedef int fixed_point;
 // Divide x by n:	x / n
 
 
-#define CONVERT_TO_FIXED_POINT(x) ((fixed_point)(x << FRACTION))
-#define CONVERT_TO_INT_TOWARD_ZERO(x) (x >> FRACTION)
-#define CONVERT_TO_INT_NEAREST(x) (x >= 0 ? ((x + (1 << (FRACTION - 1))) >> FRACTION) : ((x - (1 << (FRACTION - 1))) >> FRACTION))
-#define ADD_FIXED_POINT(x,y) (x + y)
-#define ADD_INTEGER(x,n) (x + (n << FRACTION))
-#define SUB_FIXED_POINT(x,y) (x - y)
-#define SUB_INTEGER(x,n) (x - (n << FRACTION))
-#define MULTIPLY_FIXED_POINT(x,y) (x * y)
-#define MULTIPLY_INTEGER(x,n) ((fixed_point)(((int64_t) x) * n >> FRACTION))
-#define DIVIDE_FIXED_POINT(x,y) (x / y)
-#define DIVIDE_INTEGER(x,n) ((fixed_point)((((int64_t) x) << FRACTION) / n))
+//#define CONVERT_TO_FIXED_POINT(x) ((fixed_point)(x << FRACTION))  
+//#define CONVERT_TO_INT_TOWARD_ZERO(x) (x >> FRACTION)
+//#define CONVERT_TO_INT_NEAREST(x) (x >= 0 ? ((x + (1 << (FRACTION - 1))) >> FRACTION) : ((x - (1 << (FRACTION - 1))) >> FRACTION))
+//#define ADD_FIXED_POINT(x,y) (x + y)
+//#define ADD_INTEGER(x,n) (x + (n << FRACTION))
+//#define SUB_FIXED_POINT(x,y) (x - y)
+//#define SUB_INTEGER(x,n) (x - (n << FRACTION))
+//#define MULTIPLY_FIXED_POINT(x,y) (x * y)
+//#define MULTIPLY_INTEGER(x,n) ((fixed_point)(((int64_t) x) * n >> FRACTION))
+//#define DIVIDE_FIXED_POINT(x,y) (x / y)
+//#define DIVIDE_INTEGER(x,n) ((fixed_point)((((int64_t) x) << FRACTION) / n))
+
+
+typedef int sabit_nokta;
+#define KESIR 15
+
+#define SABIT_NOKTAYA_DONUSTUR(x) ((sabit_nokta)(x << KESIR))
+#define SIFIRA_YAKIN_TAMSAYIYA_DONUSTUR(x) (x >> KESIR)
+#define EN_YAKIN_TAMSAYIYA_YUVARLA(x) (x >= 0 ? ((x + (1 << (KESIR - 1))) >> KESIR) : ((x - (1 << (KESIR - 1))) >> KESIR))
+#define SABIT_NOKTA_TOPLA(x,y) (x + y)
+#define TAM_SAYI_EKLE(x,n) (x + (n << KESIR))
+#define SABIT_NOKTA_CIKAR(x,y) (x - y)
+#define TAM_SAYI_CIKAR(x,n) (x - (n << KESIR))
+#define SABIT_NOKTA_CARP(x,y) (x * y)
+#define TAM_SAYIYLA_CARP(x,n) ((sabit_nokta)(((int64_t) x) * n >> KESIR))
+#define SABIT_NOKTA_BOL(x,y) (x / y)
+#define TAM_SAYIYLA_BOL(x,n) ((sabit_nokta)((((int64_t) x) << KESIR) / n))
+
+
+
 
 #endif /* thread/fixed_point.h */
